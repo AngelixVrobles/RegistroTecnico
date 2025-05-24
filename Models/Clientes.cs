173379f9b9eaf0a267f5f6b2db3559ac5eed8a42
Angelix Vasquez Robles <angelle120404@gmail.com>
2025-05-23 22:27:42 -0400

    Modificar CLiente Services para que guardara

--- Models/Clientes.cs
+++ Models/Clientes.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using RegistroTecnico.Services;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RegistroTecnico.Models
@@ -25,10 +26,12 @@ namespace RegistroTecnico.Models
     [Range(0, 1000000, ErrorMessage = "El límite de crédito debe estar entre 0 y 1,000,000.")]
     public decimal LimiteCredito { get; set; }
 
-    [Required(ErrorMessage = "El Campus es obligatorio.")]
+    [Required(ErrorMessage = "El Campo es obligatorio.")]
     public int TecnicoId { get; set; }
 
     [ForeignKey("TecnicoId")]
     public virtual Tecnicos Tecnicos { get; set; } = null!;
+
+
     }
 }
